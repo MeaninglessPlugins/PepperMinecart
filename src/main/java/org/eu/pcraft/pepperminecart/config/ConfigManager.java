9bdaf2ea1fcc9bbb53612fd81b5e75bac5091926
NullPepper <97275132+jlxnb@users.noreply.github.com>
2025-03-13 23:36:02 +0800

    Create ConfigManager.java

---
+++ src/main/java/org/eu/pcraft/pepperminecart/config/ConfigManager.java
@@ -0,0 +1,55 @@
+package org.eu.pcraft.powerfulfireworks.config;
+
+import lombok.Getter;
+import org.eu.pcraft.powerfulfireworks.utils.Interval;
+import org.eu.pcraft.powerfulfireworks.utils.IntervalSerializer;
+import org.spongepowered.configurate.CommentedConfigurationNode;
+import org.spongepowered.configurate.ConfigurateException;
+import org.spongepowered.configurate.yaml.NodeStyle;
+import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
+import io.leangen.geantyref.TypeToken;
+
+import java.lang.reflect.Type;
+import java.nio.file.Path;
+import java.util.function.Predicate;
+
+public class ConfigManager<T> {
+    CommentedConfigurationNode node;
+
+    YamlConfigurationLoader loader;
+
+    Class<T> configType;
+
+    @Getter
+    T configModule;
+
+    public ConfigManager(Path src, T cm){
+        loader = YamlConfigurationLoader.builder()
+                .nodeStyle(NodeStyle.BLOCK)
+                .indent(2)
+                .defaultOptions(opts -> opts.serializers(build ->
+                        build.register(TypeToken.get(Interval.class), IntervalSerializer.INSTANCE)))
+                .path(src) // Set where we will load and save to
+                .build();
+        node=loader.createNode();
+        configType = (Class<T>) cm.getClass();
+        configModule = cm;
+    }
+    public void loadConfig(){
+        try {
+            node=loader.load();
+            configModule=node.get(configType);
+            saveConfig();
+        }catch (ConfigurateException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public void saveConfig(){
+        try {
+            node.set(configModule);
+            loader.save(node);
+        }catch (ConfigurateException e){
+            throw new RuntimeException(e);
+        }
+    }
+}

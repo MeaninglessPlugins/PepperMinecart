a3c653a99b362a9668ba9f9fdefc6238fd52ed41
NullPepper <97275132+jlxnb@users.noreply.github.com>
2026-08-12 22:41:55 +0800

    Allow mobs to push block-carts
    Remove the VehicleEntityCollisionEvent handler so non-player living
    entities can push carts carrying blocks. Boarding is still blocked by
    the existing VehicleEnterEvent cancellation.

--- src/main/java/org/eu/pcraft/pepperminecart/listener/VehicleListener.java
+++ src/main/java/org/eu/pcraft/pepperminecart/listener/VehicleListener.java
@@ -1,14 +1,11 @@
 package org.eu.pcraft.pepperminecart.listener;
 
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Minecart;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.VehicleDestroyEvent;
 import org.bukkit.event.vehicle.VehicleEnterEvent;
-import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
 import org.eu.pcraft.pepperminecart.PepperMinecart;
 import org.eu.pcraft.pepperminecart.service.MinecartService;
@@ -37,17 +34,6 @@ public class VehicleListener implements Listener {
     public void onVehicleMove(VehicleMoveEvent event) {
         if (event.getVehicle() instanceof Minecart minecart) {
             service.handleDropperCartActivation(minecart, plugin.getMainConfig());
-        }
-    }
-
-    @EventHandler
-    public void onVehicleEntityCollision(VehicleEntityCollisionEvent event) {
-        // 只拦截非玩家活体生物（怪物等）进入有方块的矿车；玩家推动矿车不受影响
-        if (event.getVehicle() instanceof Minecart minecart
-                && service.getBlockItem(minecart) != null
-                && event.getEntity() instanceof LivingEntity
-                && !(event.getEntity() instanceof Player)) {
-            event.setCancelled(true);
         }
     }
 
